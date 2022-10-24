87c47a9df251401edeba4e3d5342fee707f247ef
diegoczel <diegoczel@gmail.com>
2022-10-24 07:42:56 -0300

    Conslusão

---
+++ HospedagemConsole/Models/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospedagemConsole.Models
+{
+    internal class Pessoa
+    {
+        public Pessoa() { }
+
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+    }
+}
---
+++ HospedagemConsole/Models/Suite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospedagemConsole.Models
+{
+    internal class Suite
+    {
+        public Suite() { }
+
+        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+        {
+            TipoSuite = tipoSuite;
+            Capacidade = capacidade;
+            ValorDiaria = valorDiaria;
+        }
+
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+    }
+}
